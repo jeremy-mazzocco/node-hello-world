4106791c6a5b02dfb178e6cbfa29e15d66efce0c
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-11-07 15:27:22 +0100

    bonus

--- server.js
+++ server.js
@@ -4,19 +4,30 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 let port = +process.env.PORT;
-console.log(port);
-let content = process.env.TEXT;
 
+const content = process.env.TEXT;
 
-function htmlResponse(res, content) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`<h1>${content}</h1>`);
+const sentences = [
+    "L'unica persona che può limitare il tuo successo sei tu stesso.",
+    "Il fallimento è solo l'opportunità di iniziare di nuovo in modo più intelligente.",
+    "Il successo è camminare da un fallimento all'altro senza perdere entusiasmo.",
+    "Credi in te stesso e sarai inarrestabile.",
+    "Le grandi cose non vengono mai da zone di comfort."
+];
+
+function getRandomSentence(sentences) {
+    const randomIndex = Math.floor(Math.random() * sentences.length);
+    return sentences[randomIndex];
 }
 
+function htmlResponse(res, content) {
+    const randomSentence = getRandomSentence(sentences); // Ottieni una frase casuale
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(`<h1>${content}</h1><h2>${randomSentence}</h2>`);
+}
 
 const server = http.createServer(function (req, res) {
     htmlResponse(res, content);
-
 });
 
 
